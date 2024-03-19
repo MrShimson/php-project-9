bb995c7709d60fb69dd7cd7cf3644cfd5d75226c
Семён Никифоров <semnikiforov2000@gmail.com>
2024-03-19 20:10:44 +0300

    Install html parser DiDom and add extend сheckUrl capability

--- src/Checker.php
+++ src/Checker.php
@@ -2,6 +2,8 @@
 
 namespace App;
 
+use DiDom\Document;
+
 class Checker implements CheckerInterface
 {
     private \GuzzleHttp\Client $client;
@@ -9,6 +11,20 @@ class Checker implements CheckerInterface
     public function __construct(\GuzzleHttp\Client $client)
     {
         $this->client = $client;
+    }
+
+    private function getTagDataFromHtml(string $url, string $tag): string|null
+    {
+        $body = $this->client->request('GET', $url, ['http_errors' => true])->getBody()->getContents();
+        $document = new Document($body);
+
+        if ($document->has($tag)) {
+            $data = $document->first($tag);
+        } else {
+            $data = null;
+        }
+
+        return $data;
     }
 
     public function checkUrl(string $url): array
@@ -48,6 +64,22 @@ class Checker implements CheckerInterface
         $data = [
             'status_code' => $status
         ];
+
+        $tags = [
+            'h1' => 'h1::text',
+            'title' => 'title::text',
+            'description' => 'meta[name="description"]::attr(content)'
+        ];
+
+        foreach ($tags as $key => $value) {
+            $tagData = $this->getTagDataFromHtml($url, $value);
+
+            if (is_null($tagData)) {
+                continue;
+            }
+
+            $data[$key] = $tagData;
+        }
 
         return [
             'error' => null,
